2dc7988525f2a517d279ddbac58e04055216a926
Wilton Ferreira <wiltonof@gmail.com>
2016-11-08 00:40:50 -0300

    ajustes

---
+++ bookstore-web/src/main/java/com/escribo/conf/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.escribo.conf;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	public SecurityWebApplicationInitializer() {
+		super(SecurityConfiguration.class);
+	}
+}
